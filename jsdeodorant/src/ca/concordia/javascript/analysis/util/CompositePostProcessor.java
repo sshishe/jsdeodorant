c7770b77fe8dc59db7a507e164c49e75ce436a81
Shahriar <shahriar.rostami@gmail.com>
2014-12-11 11:19:44 -0800

    Refactor two inner loops and extract two methods for finding FunctionDeclarations and AnonymousFunctionDeclarations

--- jsdeodorant/src/ca/concordia/javascript/analysis/util/CompositePostProcessor.java
+++ jsdeodorant/src/ca/concordia/javascript/analysis/util/CompositePostProcessor.java
@@ -1,5 +1,7 @@
 package ca.concordia.javascript.analysis.util;
 
+import java.util.ArrayList;
+
 import ca.concordia.javascript.analysis.abstraction.AnonymousFunctionDeclaration;
 import ca.concordia.javascript.analysis.abstraction.Function;
 import ca.concordia.javascript.analysis.abstraction.ObjectCreation;
@@ -9,54 +11,68 @@ public class CompositePostProcessor {
 	public static void processFunctionDeclarations(Program program) {
 		String fileHeader = "Invocation Type, DeclarationType, Number of Params, FunctionType";
 		CSVFileWriter.writeToFile("log.csv", fileHeader.split(","));
+		System.out.println("The number of creations in post processing: "
+				+ program.getObjectCreations().size());
 		for (ObjectCreation objectCreation : program.getObjectCreations()) {
-			for (Function functionDeclaration : program
-					.getFunctionDeclarations()) {
-				if (objectCreation.getClassName() != null)
-					if (objectCreation.getClassName().equals(
-							functionDeclaration.getName()))
-						if (objectCreation.getArguments().size() == functionDeclaration
-								.getParameters().size()) {
-							objectCreation
-									.setFunctionDeclaration(functionDeclaration);
-							StringBuilder matchedLog = new StringBuilder(
-									objectCreation.getClassName())
-									.append(",")
-									.append(functionDeclaration.getName())
-									.append(",")
-									.append(functionDeclaration.getParameters()
-											.size()).append(",")
-									.append("DECLARATION");
-
-							CSVFileWriter.writeToFile("log.csv", matchedLog
-									.toString().split(","));
-							System.out.println(matchedLog);
-						}
-			}
-			for (AnonymousFunctionDeclaration anonymousFunctionDeclaration : program
-					.getAnonymousFunctionDeclarations()) {
-
-				if (objectCreation.getClassName() != null)
-					if (objectCreation.getClassName().equals(
-							anonymousFunctionDeclaration.getName()))
-						if (objectCreation.getArguments().size() == anonymousFunctionDeclaration
-								.getParameters().size()) {
-							objectCreation
-									.setFunctionDeclaration(anonymousFunctionDeclaration);
-							StringBuilder matchedLog = new StringBuilder(
-									objectCreation.getClassName())
-									.append(",")
-									.append(anonymousFunctionDeclaration
-											.getName())
-									.append(",")
-									.append(anonymousFunctionDeclaration
-											.getParameters().size())
-									.append(",").append("EXPRESSION");
-							CSVFileWriter.writeToFile("log.csv", matchedLog
-									.toString().split(","));
-							System.out.println(matchedLog);
-						}
-			}
+			if (!findFunctionDeclaration(program, objectCreation))
+				findAnonymousFunctionDeclaration(program, objectCreation);
 		}
+	}
+
+	private static boolean findAnonymousFunctionDeclaration(Program program,
+			ObjectCreation objectCreation) {
+		for (AnonymousFunctionDeclaration anonymousFunctionDeclaration : program
+				.getAnonymousFunctionDeclarations()) {
+
+			if (objectCreation.getClassName() != null)
+				if (objectCreation.getClassName().equals(
+						anonymousFunctionDeclaration.getName()))
+					if (objectCreation.getArguments().size() == anonymousFunctionDeclaration
+							.getParameters().size()) {
+						objectCreation
+								.setFunctionDeclaration(anonymousFunctionDeclaration);
+
+						StringBuilder matchedLog = new StringBuilder(
+								objectCreation.getClassName())
+								.append(",")
+								.append(anonymousFunctionDeclaration.getName())
+								.append(",")
+								.append(anonymousFunctionDeclaration
+										.getParameters().size()).append(",")
+								.append("EXPRESSION");
+						CSVFileWriter.writeToFile("log.csv", matchedLog
+								.toString().split(","));
+						return true;
+					}
+		}
+		return false;
+	}
+
+	private static boolean findFunctionDeclaration(Program program,
+			ObjectCreation objectCreation) {
+		for (Function functionDeclaration : program.getFunctionDeclarations()) {
+			if (objectCreation.getClassName() != null)
+				if (objectCreation.getClassName().equals(
+						functionDeclaration.getName()))
+					if (objectCreation.getArguments().size() == functionDeclaration
+							.getParameters().size()) {
+						objectCreation
+								.setFunctionDeclaration(functionDeclaration);
+
+						StringBuilder matchedLog = new StringBuilder(
+								objectCreation.getClassName())
+								.append(",")
+								.append(functionDeclaration.getName())
+								.append(",")
+								.append(functionDeclaration.getParameters()
+										.size()).append(",")
+								.append("DECLARATION");
+
+						CSVFileWriter.writeToFile("log.csv", matchedLog
+								.toString().split(","));
+						return true;
+					}
+		}
+		return false;
 	}
 }
