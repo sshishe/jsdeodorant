8e90ff5eaccf3ccc425d8cf4fa83641c3cc34c7f
Shahriar Rostami Dovom <Shahriar@shahriars-mbp.wireless.concordia.ca>
2014-12-27 10:14:30 -0500

    Changed to PreservedJSCLasses.java in addition to other predefined classes

---
+++ jsdeodorant/src/ca/concordia/javascript/analysis/util/PredefinedJSClasses.java
@@ -0,0 +1,69 @@
+package ca.concordia.javascript.analysis.util;
+
+public enum PredefinedJSClasses {
+	Anchor,
+	anchors,
+	Applet,
+	applets,
+	Area,
+	Array,
+	Body,
+	Button,
+	Checkbox,
+	Date,
+	document,
+	Error,
+	EvalError,
+	FileUpload,
+	Form,
+	forms,
+	frame,
+	frames,
+	Function,
+	Hidden,
+	History,
+	history,
+	Image,
+	images,
+	Link,
+	links,
+	location,
+	Math,
+	MimeType,
+	mimetypes,
+	navigator,
+	Number,
+	Object,
+	Option,
+	options,
+	Password,
+	Plugin,
+	plugins,
+	Radio,
+	RangeError,
+	ReferenceError,
+	RegExp,
+	Reset,
+	screen,
+	Script,
+	Select,
+	String,
+	Style,
+	StyleSheet,
+	Submit,
+	SyntaxError,
+	Text,
+	Textarea,
+	TypeError,
+	URIError,
+	window;
+	
+	public static boolean contains(String token) {
+		for (PredefinedJSClasses c : PredefinedJSClasses.values()) {
+			if (c.name().equalsIgnoreCase(token)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
