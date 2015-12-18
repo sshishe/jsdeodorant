f61ec98f4a5ff7d98e8e94f653c72aeccdfaba30
Shahriar Rostami <shahriar.rostami@gmail.com>
2015-12-18 11:46:48 -0500

    Augmenting the path from require argument to precisely match modules

--- jsdeodorant/src/ca/concordia/javascript/analysis/util/ModuleHelper.java
+++ jsdeodorant/src/ca/concordia/javascript/analysis/util/ModuleHelper.java
@@ -2,6 +2,8 @@ package ca.concordia.javascript.analysis.util;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.google.javascript.jscomp.parsing.parser.trees.BinaryOperatorTree;
 import com.google.javascript.jscomp.parsing.parser.trees.CallExpressionTree;
 import com.google.javascript.jscomp.parsing.parser.trees.ExpressionStatementTree;
@@ -12,18 +14,19 @@ import com.google.javascript.jscomp.parsing.parser.trees.VariableStatementTree;
 import ca.concordia.javascript.analysis.abstraction.Module;
 
 public class ModuleHelper {
+	static Logger log = Logger.getLogger(ModuleHelper.class.getName());
 	private String moduleNameExtracted;
-	private Module module;
+	private Module currentModule;
 	private List<Module> modules;
 
 	public boolean hasRequireStatement(Module module, List<Module> modules, ParseTree expression) {
-		this.module = module;
+		this.currentModule = module;
 		this.modules = modules;
 		if (expression instanceof VariableStatementTree) {
 			VariableStatementTree variableStatement = expression.asVariableStatement();
 			for (VariableDeclarationTree variableDeclaration : variableStatement.declarations.declarations) {
 				if (checkIfInitializerIsRequireStatement(variableDeclaration)) {
-					// put the logic to locate modules here
+					findModule(moduleNameExtracted);
 					return true;
 				}
 			}
@@ -32,11 +35,20 @@ public class ModuleHelper {
 			ExpressionStatementTree expressionStatement = expression.asExpressionStatement();
 			if (expressionStatement.expression instanceof BinaryOperatorTree)
 				if (checkIfRValueIsRequireStatement(expressionStatement.expression.asBinaryOperator())) {
-					// put the logic to locate modules here
+					findModule(moduleNameExtracted);
 					return true;
 				}
 		}
 		return false;
+	}
+
+	private void findModule(String moduleName) {
+		log.warn("Module name is:" + moduleName);
+		for (Module module : modules) {
+			if (matchParts(module.getSourceFile().getOriginalPath().split("/"), moduleName.split("/"))) {
+				currentModule.addDependency(module);
+			}
+		}
 	}
 
 	private boolean checkIfRValueIsRequireStatement(BinaryOperatorTree binaryOperator) {
@@ -55,22 +67,47 @@ public class ModuleHelper {
 		if (node instanceof CallExpressionTree) {
 			CallExpressionTree callExpression = node.asCallExpression();
 			if (IdentifierHelper.getIdentifier(callExpression.operand).equals("require")) {
-				moduleNameExtracted = normalizeModuleName(IdentifierHelper
-						.getIdentifier(callExpression.arguments.arguments.get(0)).getIdentifierName());
+				moduleNameExtracted = normalizeModuleName(IdentifierHelper.getIdentifier(callExpression.arguments.arguments.get(0)).getIdentifierName());
 				return true;
 			}
-
 		}
 		return false;
 	}
 
 	private String normalizeModuleName(String moduleName) {
+		moduleName = moduleName.replace("\'", "");
+		StringBuilder normalizedPath = new StringBuilder();
 		String[] splittedModuleName = moduleName.split("/");
-		String rawModuleName;
-		if (splittedModuleName.length > 1)
-			rawModuleName = splittedModuleName[splittedModuleName.length - 1];
-		else
-			rawModuleName = splittedModuleName[0];
-		return rawModuleName.replace("\'", "");
+		for (int index = 0; index < splittedModuleName.length - 1; index++) {
+			String part = splittedModuleName[index];
+			if (part.equals(".")) {
+				String[] splittedOriginalPath = currentModule.getSourceFile().getOriginalPath().split("/");
+				String path = getElementsOf(splittedOriginalPath, 0, splittedOriginalPath.length - 2);
+				normalizedPath.append(path);
+				normalizedPath.append(getElementsOf(splittedModuleName, index + 1, splittedModuleName.length - 1));
+			}
+		}
+		return normalizedPath.toString();
+
+	}
+
+	private String getElementsOf(String[] source, int from, int to) {
+		StringBuilder path = new StringBuilder();
+		for (int index = from; index <= to; index++) {
+			path.append(source[index]);
+			if (from != to)
+				path.append("/");
+		}
+		return path.toString();
+	}
+
+	private boolean matchParts(String[] existingModuleParts, String[] nameParts) {
+		for (int pathLevel = nameParts.length - 1; pathLevel > 0; pathLevel--) {
+			for (int pathLevelForExistingModule = existingModuleParts.length - 1; pathLevelForExistingModule > nameParts.length; pathLevelForExistingModule--) {
+				if (!nameParts[pathLevel].equals(existingModuleParts[pathLevelForExistingModule]))
+					return false;
+			}
+		}
+		return true;
 	}
 }
