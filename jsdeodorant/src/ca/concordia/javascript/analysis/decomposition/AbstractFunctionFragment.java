dcfd1af4018b3335da3412cd490145519a4355b0
Shahriar Rostami <shahriar.rostami@gmail.com>
2016-01-17 15:08:50 -0500

    Fixed parent/child hierachy relation for creation of AbstractExpression on FunctionInvocation

--- jsdeodorant/src/ca/concordia/javascript/analysis/decomposition/AbstractFunctionFragment.java
+++ jsdeodorant/src/ca/concordia/javascript/analysis/decomposition/AbstractFunctionFragment.java
@@ -57,7 +57,7 @@ public abstract class AbstractFunctionFragment {
 			}
 			AbstractIdentifier identifier = IdentifierHelper.getIdentifier(callExpression.operand);
 
-			FunctionInvocation functionInvocationObject = new FunctionInvocation(callExpression, identifier, new AbstractExpression(callExpression.operand), arguments);
+			FunctionInvocation functionInvocationObject = new FunctionInvocation(callExpression, identifier, new AbstractExpression(callExpression.operand,parent), arguments);
 			addFunctionInvocation(functionInvocationObject);
 		}
 	}
